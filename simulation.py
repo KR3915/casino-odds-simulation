b6a86e329da1a02508789f1a5a4c6bffcdaad3f5
KRP395 <ondrej.brdek@gmail.com>
2025-04-11 16:56:18 +0200

    game extraction

--- simulation.py
+++ simulation.py
@@ -2,14 +2,29 @@ import json
 import random
 result = ""
 settings = []
-def extract_value():
+#extracts game settings from JSON file
+def extract_game():
+    bet = 0
+    turns = 0
+    balance = 0
+    stand = 0
+    with open("settings.json") as f:
+        settings = json.load(f)
+        for game in settings["games"]:
+                turns = game["turns"]
+                bet = game["bet"]
+                balance = game["balance"]
+                stand = game["stand at"]
+    return (turns,bet,balance,stand)      
+
+#extracts card data from the JSON file
+def extract_card():
     values_all = []
     temp_var = ""
     stringval = ""
     with open("settings.json") as file:
         settings = json.load(file)
-        card_names = [card for card in settings['cards']]
-        print("Card Names:")
+        card_names = [card for card in settings["cards"]]
         for card in card_names:
             cardvar = "card" + card["name"]
             locals()[cardvar] = (card["name"], card["value"], card["quantity"])
@@ -20,11 +35,12 @@ def extract_value():
                 temp_var = ""
         return values_all
 #random vlaue
-values_list = extract_value()
-print(values_list)
-def generate_value():
-    values_list = extract_value
-
+def extract_card():
+    values_list = extract_card+()
+    generated_val = values_list[random.randint(0,len(values_list) - 1)]
+    print (generated_val)
+game = extract_game()
+print(game)
         
         
 
