c5d32359209d4933a3d046493fcf14dd4fe54b7c
KRP395 <ondrej.brdek@gmail.com>
2025-04-11 13:22:07 +0200

    load cards into tuple

--- simulation.py
+++ simulation.py
@@ -1,5 +1,14 @@
 import json
+import random
+result = ""
+with open("settings.json") as file:
+    settings = json.load(file)
+    card_names = [card for card in settings['cards']]
+    print("Card Names:")
+    for card in card_names:
+        cardvar = "card" + card["name"]
+        locals()[cardvar] = (card["name"], card["value"], card["quantity"])
+        print (cardA)
+        
+        
 
-with open('settings.json') as f:
-    d = json.load(f)
-    print(d)
